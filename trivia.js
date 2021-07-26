97941ae895d1eb95c8a9d03c07b8ec37e34f043e
Denis Murphy <djmurphy2001@gmail.com>
2021-07-25 22:57:12 -0230

    Cycling of questions

--- trivia.js
+++ trivia.js
@@ -1,12 +1,17 @@
 "use strict";
 const $ = selector => document.querySelector(selector)
 
-let i
-let card
+let correct_answers = 0
+let question_number = 0
 
 $("#start_page").style.display = "block"
 $("#question_page").style.display = "none"
 $("#score_page").style.display = "none"
+
+const start_game = () => {
+    $("#start_page").style.display = "none"
+    $("#question_page").style.display = "block"
+}
 
 const restart = () => {
     $("#start_page").style.display = "block"
@@ -15,45 +20,41 @@ const restart = () => {
 const show_score = () => {  
     $("#question_page").style.display = "none"
     $("#score_page").style.display = "block"
-}
-const play_game = () => {
-    $("#start_page").style.display = "none"
-    $("#question_page").style.display = "block"
+
+    
 }
 
-const get_random = () => {
-
-}
-
-const change_question = () => {
-    let question = document.getElementById("question")
-    question.innerHTML = "Question 2"
-}
 
 const submit_answer = () => {
-
-    let right_answer = "answer1"
-    let correct_answers = 0
-
     let player_answer = ""
     let radios = document.getElementsByName("button")
+    let right_answer = "answer1"
 
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            player_answer = radios[i].value
-            break
+    if (question_number <= 9) {
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                console.log(radios[i].value)
+                player_answer = radios[i].value
+                break
+            }
         }
+        if (player_answer == right_answer) {
+            correct_answers += 1
+        }
+        question_number++
     }
-    
-    if (player_answer == right_answer) {
-        correct_answers += 1
+    else {
+        show_score()
     }
 
-    show_score()
+    console.log(question_number)
+    console.log(player_answer)
+    console.log(right_answer)
+    console.log(correct_answers)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     $("#submit").addEventListener("click", submit_answer)
-    $("#start").addEventListener("click", play_game)
+    $("#start").addEventListener("click", start_game)
     $("#restart").addEventListener("click", restart)
 });
