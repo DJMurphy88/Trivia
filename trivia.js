7f629ec2c4299ae917865f9f799f467d2aba6719
Denis Murphy <djmurphy2001@gmail.com>
2021-08-01 09:30:01 -0230

    site layout changes

--- trivia.js
+++ trivia.js
@@ -4,6 +4,29 @@ const $ = selector => document.querySelector(selector)
 let correct_answers = 0
 let question_number = 0
 
+let questions = [
+    {question: "question1",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
+    {question: "question2",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"},
+    {question: "question3",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "c"},
+    {question: "question4",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "d"},
+    {question: "question5",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
+    {question: "question6",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"},
+    {question: "question7",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "c"},
+    {question: "question8",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "d"},
+    {question: "question9",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
+    {question: "question10",
+     answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"}
+    ]
+
 $("#start_page").style.display = "block"
 $("#question_page").style.display = "none"
 $("#score_page").style.display = "none"
@@ -18,47 +41,31 @@ const restart = () => {
     $("#start_page").style.display = "block"
     $("#score_page").style.display = "none"
 }
-const show_score = () => {  
+const show_score = () => {
+    question_number = 0
+    $("#score_span").innerHTML = `You got ${correct_answers} out of 10 correct.`
     $("#question_page").style.display = "none"
     $("#score_page").style.display = "block"   
 }
 
+const question_randomizer = () => {
+// Starting with the last question, swapping it with random question 'below' it
+    for (let i = questions.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [questions[i], questions[j]] = [questions[j], questions[i]]
+    }
+}
+
 const set_question = () => {
-    let i = Math.floor(Math.random() * 10);
-
-   let questions = [
-        {question: "question1",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
-        {question: "question2",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"},
-        {question: "question3",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "c"},
-        {question: "question4",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "d"},
-        {question: "question5",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
-        {question: "question6",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"},
-        {question: "question7",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "c"},
-        {question: "question8",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "d"},
-        {question: "question9",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "a"},
-        {question: "question10",
-         answers: {a: "a", b: "b", c: "c", d: "d"}, right_answer: "b"}
-        ]
-
-       let random_question = questions[i]
-       console.log(random_question)
-
-    $("#question").value = random_question.right_answer
-    $("#question").innerHTML = random_question.question    
+    $("#info_panel").innerHTML = "Question " +(question_number+1)+ "  of 10."
+    
+    $("#question").value = questions[question_number].right_answer
+    $("#question").innerHTML = questions[question_number].question    
  
-    $("#A1").nextElementSibling.innerHTML = random_question.answers.a
-    $("#A2").nextElementSibling.innerHTML = random_question.answers.b
-    $("#A3").nextElementSibling.innerHTML = random_question.answers.c
-    $("#A4").nextElementSibling.innerHTML = random_question.answers.d
+    $("#A1").nextElementSibling.innerHTML = questions[question_number].answers.a
+    $("#A2").nextElementSibling.innerHTML = questions[question_number].answers.b
+    $("#A3").nextElementSibling.innerHTML = questions[question_number].answers.c
+    $("#A4").nextElementSibling.innerHTML = questions[question_number].answers.d
 }
 
 const submit_answer = () => {
@@ -67,7 +74,7 @@ const submit_answer = () => {
 
     let right_answer = document.getElementById("question").value
 
-    if (question_number <= 9) {
+    if (question_number < 9) {
         for (let i = 0; i < radios.length; i++) {
             if (radios[i].checked) {
                 player_answer = radios[i].value
@@ -78,19 +85,17 @@ const submit_answer = () => {
             correct_answers += 1
         }
         question_number++
+
+        
         play_game()
     }
     else {
         show_score()
     }
-
-    console.log("Question number: " +question_number)
-    console.log("Player answer: " +player_answer)
-    console.log("Right answer: " +right_answer)
-    console.log("Correct answer: " +correct_answers)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    question_randomizer()
     $("#submit").addEventListener("click", submit_answer)
     $("#start").addEventListener("click", play_game)
     $("#restart").addEventListener("click", restart)
